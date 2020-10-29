2c38be7826ae1421dfdd8782c7544e8bf34cdc6a
Aitmyrza <omuraliev2017@bk.ru>
2020-10-29 20:41:10 +0600

    added security in user profile

--- user_account/views.py
+++ user_account/views.py
@@ -1,8 +1,17 @@
-from django.contrib import auth
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 
+@login_required
 def profile(request, id):
-    """Личный кабинет персонального пользователя"""
-    context = {'user': auth.models.User.objects.get(id=id)}
+    """
+    Личный кабинет персонального пользователя.
+    При попытке входа в чужой профиль, сервер переадресует пользователя на свой собственный.
+    """
+    user = User.objects.get(id=id)
+    if request.user.id != user.id:
+        return redirect('profile', id=request.user.id)
+
+    context = {'user': user}
     return render(request, 'profile.html', context)
