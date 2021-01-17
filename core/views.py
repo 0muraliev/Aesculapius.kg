0ef6627fc7ed8e06fec6d6bb92b44991d92c562b
Aitmyrza Omuraliev <omuraliev2017@bk.ru>
2021-01-17 23:43:10 +0600

    Fix a mistake on the home page when there is no doctor's photo.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 from user_account.models import Doctor
@@ -5,5 +6,12 @@ from user_account.models import Doctor
 
 def home(request):
     """Главная страница."""
-    doctors = Doctor.objects.filter(user__is_active=True)
+    # Для вывода доктора на главной странице,
+    # необходимы активный статус доктора, биография и его имя или фамилия.
+    doctors = Doctor.objects.filter(
+        Q(user__is_active=True),
+        ~Q(biography=''),
+        ~Q(user__first_name='') |
+        ~Q(user__last_name='')
+    )
     return render(request, 'core/home.html', {'doctors': doctors})
