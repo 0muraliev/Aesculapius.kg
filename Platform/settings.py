6e4c8cbbd89ece81bbc937636108ee915a8d4fa7
Aitmyrza Omuraliev <omuraliev2017@bk.ru>
2021-02-17 13:30:50 +0600

    Add host to settings.

--- Platform/settings.py
+++ Platform/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://aesculapius-kg.herokuapp.com/', '.herokuapp.com', ]
 
 
 # Application definition
